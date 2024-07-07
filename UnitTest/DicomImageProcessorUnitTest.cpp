dfc7ce6779c3e35f3a36ad001fc27a6e1450808d
yixiang <yixiang.sin@gbgplc.com>
2024-07-07 20:40:48 +0800

    [Pixel Threshold Detection] Create Unit Test for Threshold Detection

--- UnitTest/DicomImageProcessorUnitTest.cpp
+++ UnitTest/DicomImageProcessorUnitTest.cpp
@@ -17,6 +17,9 @@ public:
         cv::Rect edgeDetectionIndicatorRoiDetector(const cv::Mat& input) override {
                 return DicomImageProcessor::edgeDetectionIndicatorRoiDetector(input);
         }
+        cv::Rect pixelThresholdDetectionIndicatorRoiDetector(const cv::Mat& input) override {
+                return DicomImageProcessor::pixelThresholdDetectionIndicatorRoiDetector(input);
+        }
 };
 
 // Example test that always passes
@@ -48,6 +51,32 @@ TEST(DicomImageProcessorTest, edgeDetectionIndicatorRoiDetectorEmptyImage) {
         EXPECT_EQ(roi.y, -1);
         EXPECT_EQ(roi.width, -1);
         EXPECT_EQ(roi.height,-1);
+}
+
+TEST(DicomImageProcessorTest, pixelThresholdDetectionIndicatorRoiDetector) {
+        DicomImageProcessorTest dicomImageProcessor;
+
+        // Check if the detected ROI is close to the top left indicator
+        cv::Mat inputTopLeft = cv::imread("../../../../dicom/topLeft.jpeg", 0);
+        cv::Rect intersectTopLeft = dicomImageProcessor.pixelThresholdDetectionIndicatorRoiDetector(inputTopLeft) & topLeftIndicator;
+        EXPECT_GT(intersectTopLeft.area(), topLeftIndicator.area() * 0.9);
+
+        // Check if the detected ROI is close to the bottom right indicator
+        cv::Mat inputBottomRight = cv::imread("../../../../dicom/bottomRight.png", 0);
+        cv::Rect intersectBottomRight = dicomImageProcessor.pixelThresholdDetectionIndicatorRoiDetector(inputBottomRight) & bottomRightIndicator;
+        EXPECT_GT(intersectBottomRight.area(), topLeftIndicator.area() * 0.9);
+}
+
+TEST(DicomImageProcessorTest, pixelThresholdDetectionIndicatorRoiDetectorEmptyImage) {
+        DicomImageProcessorTest dicomImageProcessor;
+        cv::Mat emptyInput;
+
+        // Check if the function returns an negative ROI for an empty image
+        cv::Rect roi = dicomImageProcessor.pixelThresholdDetectionIndicatorRoiDetector(emptyInput);
+        EXPECT_EQ(roi.x, -1);
+        EXPECT_EQ(roi.y, -1);
+        EXPECT_EQ(roi.width, -1);
+        EXPECT_EQ(roi.height, -1);
 }
 
 // Main function
