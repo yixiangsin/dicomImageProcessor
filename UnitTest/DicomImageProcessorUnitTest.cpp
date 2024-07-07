99c6e3d3b1ac1fb836ccd5646d07da8abd01faa6
yixiang <yixiang.sin@gbgplc.com>
2024-07-07 17:33:05 +0800

    [Edge Detection] commit unit test for edge Detection commit main.cpp for running main program commit edgeDetectionIndicatorRoiDetector full function

---
+++ UnitTest/DicomImageProcessorUnitTest.cpp
@@ -0,0 +1,57 @@
+#include <gtest/gtest.h>
+#include <opencv2/opencv.hpp>
+#include "DicomImageProcessor.h"
+
+cv::Rect topLeftIndicator(108, 90, 15, 15);
+cv::Rect topRightIndicator(515, 90, 15, 15);
+cv::Rect bottomLeftIndicator(108, 374, 15, 15);
+cv::Rect bottomRightIndicator(515, 374, 15, 15);
+
+class DicomImageProcessorTest : public ::DicomImageProcessor {
+
+public:
+        DicomImageProcessorTest() {
+        }
+        ~DicomImageProcessorTest() {
+        }
+        cv::Rect edgeDetectionIndicatorRoiDetector(const cv::Mat& input) override {
+                return DicomImageProcessor::edgeDetectionIndicatorRoiDetector(input);
+        }
+};
+
+// Example test that always passes
+TEST(ExampleTest, AlwaysPass) {
+        EXPECT_EQ(1, 1);
+}
+
+TEST(DicomImageProcessorTest, edgeDetectionIndicatorRoiDetector) {
+        DicomImageProcessorTest dicomImageProcessor;
+
+        // Check if the detected ROI is close to the top left indicator
+        cv::Mat inputTopLeft = cv::imread("../../../../dicom/topLeft.jpeg", 0);
+        cv::Rect intersectTopLeft = dicomImageProcessor.edgeDetectionIndicatorRoiDetector(inputTopLeft) &  topLeftIndicator;
+        EXPECT_GT(intersectTopLeft.area(), topLeftIndicator.area()* 0.9);
+
+        // Check if the detected ROI is close to the bottom right indicator
+        cv::Mat inputBottomRight = cv::imread("../../../../dicom/bottomRight.png", 0);
+        cv::Rect intersectBottomRight = dicomImageProcessor.edgeDetectionIndicatorRoiDetector(inputBottomRight) & bottomRightIndicator;
+        EXPECT_GT(intersectBottomRight.area(), topLeftIndicator.area() * 0.9);
+}
+
+TEST(DicomImageProcessorTest, edgeDetectionIndicatorRoiDetectorEmptyImage) {
+        DicomImageProcessorTest dicomImageProcessor;
+        cv::Mat emptyInput;
+
+        // Check if the function returns an negative ROI for an empty image
+        cv::Rect roi = dicomImageProcessor.edgeDetectionIndicatorRoiDetector(emptyInput);
+        EXPECT_EQ(roi.x, -1);
+        EXPECT_EQ(roi.y, -1);
+        EXPECT_EQ(roi.width, -1);
+        EXPECT_EQ(roi.height,-1);
+}
+
+// Main function
+int main(int argc, char** argv) {
+        ::testing::InitGoogleTest(&argc, argv);
+        return RUN_ALL_TESTS();
+}
